7eaa7f628062bde659d163fcab8ed2e22b8c75e7
Marukin-tnk <67319010014@tnk.ac.th>
2024-11-06 23:14:28 +0700

    ใบงานที่ 3 Marukin

---
+++ Python_week04/test for.py
@@ -0,0 +1,24 @@
+a = 0
+for i in range(1,10,1):
+        print(i)
+        a +=i
+        i +=a
+        break
+        
+a = 0
+for i in range(1,10,1):
+        if i == 5:
+            range
+        print(i)
+
+a = 0
+for i in range(1,10,1):
+        if i == 5: 
+            continue
+        print(i)
+        
+a = 0
+for i in range(1,10,1):
+        if i % 2 == 0: 
+            continue
+        print(i)
---
+++ Python_week04/test while.py
@@ -0,0 +1,11 @@
+a = 1
+while a <= 10:
+    print(f"รอบที่: {a}")
+    a +=1
+
+a = 1
+while True:
+    print(f"รอบที่: {a}")
+    a +=1
+    if a == 21:
+        break
